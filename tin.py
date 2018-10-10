9236c81b19d162ada3dd165880d007fa94faa7c2
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-10-10 20:07:51 +0530

    Update tin.py

--- tin.py
+++ tin.py
@@ -173,3 +173,57 @@ val_images, val_labels, val_files = load_validation_images(VAL_IMAGES_DIR, val_d
 val_labels_encoded = training_le.transform(val_labels)
 plot_objects(val_images[0:30])
 print (val_labels_encoded[0:30])
+
+height = IMAGE_SIZE
+width = IMAGE_SIZE
+channels = NUM_CHANNELS
+n_inputs = height * width * channels
+n_outputs = 200
+
+reset_graph()
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs], name="X")
+X_reshaped = tf.reshape(X, shape=[-1, height, width, channels])
+y = tf.placeholder(tf.int32, shape=[None], name="y")
+
+#input shape [-1, 64, 64, 3]
+conv1 = tf.layers.conv2d(
+            inputs=X_reshaped, 
+            filters=32, 
+            kernel_size=[5,5],
+            padding='SAME',
+            activation=tf.nn.relu, 
+            name="conv1")
+
+#shape after conv1: [-1, 64, 64, 32]
+pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+conv2 = tf.layers.conv2d(
+            inputs=pool1, 
+            filters=64,
+            kernel_size=[5,5],
+            padding='SAME',
+            activation=tf.nn.relu, 
+            name="conv2")
+
+pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+# Dense Layer
+pool2_flat = tf.reshape(pool2, [-1, 8 * 8 * 64])
+dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+dropout = tf.layers.dropout(inputs=dense, rate=0.4)
+dropout_reshape = tf.reshape(dropout, [-1, 8 * 8 * 64])
+
+# Logits Layer
+logits = tf.layers.dense(inputs=dropout_reshape, units=200, name='output')
+Y_proba = tf.nn.softmax(logits, name="Y_proba")
+
+xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
+loss = tf.reduce_mean(xentropy)
+optimizer = tf.train.AdamOptimizer()
+training_op = optimizer.minimize(loss)
+
+correct = tf.nn.in_top_k(logits, y, 1)
+accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
