ab0622b398fb284bc1ee69e72fddaa2f6e630b0f
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-10-10 19:37:33 +0530

    Update tin.py

--- tin.py
+++ tin.py
@@ -1,1 +1,12 @@
+import os
+import matplotlib
+import numpy as np
+import pandas as pd
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow as tf
+import zipfile
+import requests, io
+from sklearn import preprocessing
 
