d3e8cfecdeb0f4d323ac599d0660790ec9e0de90
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-10-10 19:38:17 +0530

    Update tin.py

--- tin.py
+++ tin.py
@@ -10,3 +10,27 @@ import zipfile
 import requests, io
 from sklearn import preprocessing
 
+BATCH_SIZE = 20
+NUM_CLASSES = 200
+NUM_IMAGES_PER_CLASS = 500
+NUM_IMAGES = NUM_CLASSES * NUM_IMAGES_PER_CLASS
+TRAINING_IMAGES_DIR = './tiny-imagenet-200/train/'
+TRAIN_SIZE = NUM_IMAGES
+
+NUM_VAL_IMAGES = 10000
+VAL_IMAGES_DIR = './tiny-imagenet-200/val/'
+
+IMAGE_SIZE = 64
+NUM_CHANNELS = 3
+IMAGE_ARR_SIZE = IMAGE_SIZE * IMAGE_SIZE * NUM_CHANNELS
+IMAGES_URL = 'http://cs231n.stanford.edu/tiny-imagenet-200.zip'
+
+def download_images(url):
+    if (os.path.isdir(TRAINING_IMAGES_DIR)):
+        print ('Images already downloaded...')
+        return
+    r = requests.get(url, stream=True)
+    print ('Downloading ' + url )
+    zip_ref = zipfile.ZipFile(io.StringIO(r.content))
+    zip_ref.extractall('./')
+    zip_ref.close()
