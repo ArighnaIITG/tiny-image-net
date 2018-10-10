8a977664296204166b9d66f8fa395ff5677a49a5
Arighna Chakrabarty <arighna.chakrabarty100@gmail.com>
2018-10-10 19:44:37 +0530

    Update tin.py

--- tin.py
+++ tin.py
@@ -34,3 +34,120 @@ def download_images(url):
     zip_ref = zipfile.ZipFile(io.StringIO(r.content))
     zip_ref.extractall('./')
     zip_ref.close()
+
+def load_training_images(image_dir, batch_size=500):
+
+    image_index = 0
+    
+    images = np.ndarray(shape=(NUM_IMAGES, IMAGE_ARR_SIZE))
+    names = []
+    labels = []                       
+    
+    # Loop through all the types directories
+    for type in os.listdir(image_dir):
+        if os.path.isdir(image_dir + type + '/images/'):
+            type_images = os.listdir(image_dir + type + '/images/')
+            # Loop through all the images of a type directory
+            batch_index = 0;
+            #print ("Loading Class ", type)
+            for image in type_images:
+                image_file = os.path.join(image_dir, type + '/images/', image)
+
+                # reading the images as they are; no normalization, no color editing
+                image_data = mpimg.imread(image_file) 
+                #print ('Loaded Image', image_file, image_data.shape)
+                if (image_data.shape == (IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)):
+                    images[image_index, :] = image_data.flatten()
+
+                    labels.append(type)
+                    names.append(image)
+                    
+                    image_index += 1
+                    batch_index += 1
+                if (batch_index >= batch_size):
+                    break;
+                    
+    return (images, np.asarray(labels), np.asarray(names))
+
+def get_label_from_name(data, name):
+    for idx, row in data.iterrows():       
+        if (row['File'] == name):
+            return row['Class']
+        
+    return None
+
+
+def load_validation_images(testdir, validation_data, batch_size=NUM_VAL_IMAGES):
+    labels = []
+    names = []
+    image_index = 0
+    
+    images = np.ndarray(shape=(batch_size, IMAGE_ARR_SIZE))
+    val_images = os.listdir(testdir + '/images/')
+           
+    # Loop through all the images of a val directory
+    batch_index = 0;
+    
+    
+    for image in val_images:
+        image_file = os.path.join(testdir, 'images/', image)
+        #print (testdir, image_file)
+
+        # reading the images as they are; no normalization, no color editing
+        image_data = mpimg.imread(image_file) 
+        if (image_data.shape == (IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)):
+            images[image_index, :] = image_data.flatten()
+            image_index += 1
+            labels.append(get_label_from_name(validation_data, image))
+            names.append(image)
+            batch_index += 1
+            
+        if (batch_index >= batch_size):
+            break;
+    
+    print ("Loaded Validation images ", image_index)
+    return (images, np.asarray(labels), np.asarray(names))
+   
+    
+
+def plot_object(data):
+    plt.figure(figsize=(1,1))
+    image = data.reshape(IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)
+    plt.imshow(image, cmap = matplotlib.cm.binary,
+               interpolation="nearest")
+    plt.axis("off")
+    plt.show()
+
+def plot_objects(instances, images_per_row=10, **options):
+    size = IMAGE_SIZE
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size,size,NUM_CHANNELS) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        if (row == len(instances)/images_per_row):
+            break
+        rimages = images[row * images_per_row : (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, **options)
+    plt.axis("off")
+    plt.show()
+    
+def get_next_batch(batchsize=50):
+    for cursor in range(0, len(training_images), batchsize):
+        batch = []
+        batch.append(training_images[cursor:cursor+batchsize])
+        batch.append(training_labels_encoded[cursor:cursor+batchsize])       
+        yield batch
+
+def get_next_labels(batchsize=50):
+    for cursor in range(0, len(training_images), batchsize):
+        yield training_labels_encoded[cursor:cursor+batchsize]  
+    
+def reset_graph(seed=42):
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    np.random.seed(seed
